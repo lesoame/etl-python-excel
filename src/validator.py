3d377518369013825e9dfe1aae2c7cf95044e663
lesoame <leunip_2010@hotmail.com>
2025-03-24 16:38:53 -0300

    update

--- src/validator.py
+++ src/validator.py
@@ -11,9 +11,9 @@ class SalesSheet(BaseModel):
     Date: date = Field(..., description="Data da entrada no formato YYYY-MM-DD")
     AdSet_name: str = Field(..., description="Nome do conjunto de anúncios")
     Amount_spent: float = Field(0.0, ge=0, le=1200, description="Valor gasto no anúncio (mínimo 0, máximp 1200)")  # Ensuring non-negative values
-    Link_clicks: Optional[int] = Field(None, description="Número de cliques no link", nullable=True)
-    Impressions: int = Field(..., description="Número de impressões do anúncio")
-    Conversions: Optional[int] = Field(None, description="Número de conversões registradas")
-    Segmentacao: str = Field(..., description="Segmentação usada no anúncio")
+    Link_clicks: Optional[float] = Field(None, description="Número de cliques no link", nullable=True)
+    Impressions: Optional[float] = Field(..., description="Número de impressões do anúncio")
+    Conversions: Optional[float] = Field(None, description="Número de conversões registradas")
+    Segmentacao: Optional[float] = Field(..., description="Segmentação usada no anúncio")
     Tipo_de_Anuncio: str = Field(..., description="Tipo de anúncio")
     Fase: str = Field(..., description="Fase da campanha")
