75701a405b4cd73ee5177eb14bda2cd263608159
lesoame <leunip_2010@hotmail.com>
2025-03-24 12:27:56 -0300

    adding output.html

---
+++ main.py
@@ -0,0 +1,13 @@
+# import libs
+
+import pandas as pd
+from ydata_profiling import ProfileReport
+
+# Load data
+df = pd.read_csv('data.csv')
+
+# Generate report
+profile = ProfileReport(df, title="Padas Profile Report")
+
+# Save report to file
+profile.to_file("output.html")
